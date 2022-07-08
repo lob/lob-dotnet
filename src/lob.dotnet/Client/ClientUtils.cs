3a43b870e33e32e9142d939ab40dcff6ff19f81f
Jessica Ho <45194378+jho44@users.noreply.github.com>
2022-07-08 11:16:06 -0700

    letters integration+unit tests (#9)
    * letters integration+unit tests
    
    * chore: clarify todos
    
    * chore: clarify todos
    
    Co-authored-by: BennyKitchell <bennykitchell@gmail.com>

--- src/lob.dotnet/Client/ClientUtils.cs
+++ src/lob.dotnet/Client/ClientUtils.cs
@@ -1,7 +1,7 @@
 /*
  * Lob
  *
- * The Lob API is organized around REST. Our API is designed to have predictable, resource-oriented URLs and uses HTTP response codes to indicate any API errors. <p> Looking for our [previous documentation](https://lob.github.io/legacy-docs/)? 
+ * The Lob API is organized around REST. Our API is designed to have predictable, resource-oriented URLs and uses HTTP response codes to indicate any API errors. <p> Looking for our [previous documentation](https://lob.github.io/legacy-docs/)?
  *
  * The version of the OpenAPI document: 1.3.0
  * Contact: lob-openapi@lob.com
@@ -54,7 +54,7 @@ namespace lob.dotnet.Client
                     if(collection.Count == 1) {
                         parameters.Add(name, ParameterToString(item));
                     }
-                    
+                           
                     parameters.Add(name, ParameterToString(item));
                 }
             }
